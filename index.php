8b9f4324952664f3746e0bf30f316b76c114ecb2
MichelangeloCaltagirone <michelangelocaltagirone@gmail.com>
2024-10-03 16:16:49 +0200

    Add stars for express the Vote of the Hotel

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Font Awensome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
@@ -65,7 +67,9 @@
 
             <ul>
                 <li class="p-2">
-                    <span class="fw-bold pb-5">Voto: <?= $hotel['vote'] ?></span><br>
+                    <span class="fw-bold pb-5">Voto: <?php 
+                        for($i = 0 ; $i < $hotel['vote'] ;$i++ ) { echo '<i class="fa-solid fa-star text-warning"></i>';} ?>
+                    </span><br>
                     <p class="fs-4"><?= $hotel['description'] ?></p>
                 </li>
                 <li>
